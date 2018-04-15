e4a6945d10e12294b36dfddd1c231fbd3326b075
bigreni <bigreni@gmail.com>
2018-04-15 17:43:33 -0500

    v1 fixes 8

--- www/js/favorites.js
+++ www/js/favorites.js
@@ -15,7 +15,7 @@
   } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
     admobid = {
       banner: 'ca-app-pub-1683858134373419/7790106682', // or DFP format "/6253334/dfp_example_ad"
-      interstitial: 'ca-app-pub-9249695405712287/7516860115'
+      interstitial: 'ca-app-pub-9249695405712287/7187939394'
     };
   }
 
